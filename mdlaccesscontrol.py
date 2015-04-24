765256404c127abbe886be5b576dafd576a43699
aldrix <aldrix_marfil@hotmail.com>
2015-04-24 15:51:03 -0430

    Agregar casos de prueba al archivo de check_password y cambio de nombre de los modulos. Cambio de expresion regular en mdlaccesscontrol.py

--- mdlaccesscontrol.py
+++ mdlaccesscontrol.py
@@ -16,44 +16,55 @@ class clsAccessControl(object):
         # Verificar la longitud del password
         oHash=""
         olength_password=self.length_password(value)
-
-        if re.match('^(?=(\S)*\d)(?=(\S)*[a-z]*)(?=(\S)*[A-Z])(?=(\S)*[@\.#\$\+\*]).{8,16}$',value):
+        
+        verificar = re.match('(([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[@.#$+*])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*\d)|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*[A-Z])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*[@.#$+*])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[A-Z])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*\d)',value) 
+        if verificar and olength_password>=8 and olength_password<=16:
             # uuid es usado para generar numeros random
             salt = uuid.uuid4().hex
             # hash
             oHash= hashlib.sha256(salt.encode() + value.encode()).hexdigest() + ':' + salt
         else:
-            print('El Password debe contener entre 8 y 16 caracteres')
+            print('El Password debe contener entre 8 y 16 caracteres. Debe contener al menos una mayúscula,')
+            print(' un número y un caracter especial')
         return oHash   
     
     def check_password(self, oPassworkEncript, oCheckPassword):
         # Verificar la longitud del password
         olength_password=self.length_password(oCheckPassword)
-        if olength_password>=8 and olength_password<=16: 
-            # uuid es usado para generar numeros random
+        
+        verificar = re.match('(([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[@.#$+*])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*\d)|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*[A-Z])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*[@.#$+*])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*\d([0-9a-zA-Z]|[@.#$+*])*[A-Z])|'+
+                           '(([0-9a-zA-Z]|[@.#$+*])*[@.#$+*]([0-9a-zA-Z]|[@.#$+*])*[A-Z]([0-9a-zA-Z]|[@.#$+*])*\d)',value) 
+        
+        if verificar and olength_password>=8 and olength_password<=16: 
+            
             oPassworkEncript, salt = oPassworkEncript.split(':')
             return oPassworkEncript == hashlib.sha256(salt.encode() + oCheckPassword.encode()).hexdigest()
         else:
-            print('El Password no posee la cantidad de caracteres requerida')
+            print('El Password no posee la cantidad de caracteres requerida o los caracteres requeridos Ej: @.#$+*')
             return False
     
     def length_password(self, user_password):
-        # uuid es usado para generar numeros random
         return len(user_password)
 
-<<<<<<< HEAD
 #Para encriptar un passwork  
 #oPassword = input('Por favor ingrese su password: ')
 #Se crea un objeto tipo clsAccessControl
 #oAccessControl=clsAccessControl()
 #oPassworkEncript = oAccessControl.encript(oPassword)
-=======
 # #Para encriptar un passwork  
 # oPassword = input('Por favor ingrese su password: ')
 #  #Se crea un objeto tipo clsAccessControl
 # oAccessControl=clsAccessControl()
 # oPassworkEncript = oAccessControl.encript(oPassword)
->>>>>>> refs/heads/master
 # print('El Password almacenado en la memoria es: ' + oPassworkEncript)
 # if oPassworkEncript:
 #      #Para validar el passwork introducido
